dc5d1ebde6a159af5fc9695179c184bd65932db9
YourUsername <your@email.com>
2026-06-08 23:17:51 +0000

    Fix: Explicit remappings to resolve pathing errors

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
 import "forge-std/Script.sol";
 import "src/NexusTokenRegistry.sol";
-import "src/Assets.sol";
 
 contract DeployScript is Script {
     function run() external {
-        vm.startBroadcast();
-        new NexusTokenRegistry(msg.sender);
-        new Assets();
+        uint256 pk = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(pk);
+        new NexusTokenRegistry(0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE, 0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE);
         vm.stopBroadcast();
     }
 }
