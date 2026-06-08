5a244790dd2cc42975ea8fe7f57e428b6f9a217e
Charles Henderson <Charles.henderson34@icloud.com>
2026-06-08 22:34:57 +0000

    Fix: Isolate clean project structure for Foundry deployment

--- my-production-vault/script/Deploy.s.sol
+++ my-production-vault/script/Deploy.s.sol
@@ -2,15 +2,13 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/NexusTokenRegistry.sol";
+import "src/NexusTokenRegistry.sol";
 
 contract DeployScript is Script {
     function run() external {
         uint256 pk = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(pk);
-        
         new NexusTokenRegistry(0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE, 0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE);
-        
         vm.stopBroadcast();
     }
 }
