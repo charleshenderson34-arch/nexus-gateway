15e223433033420453929b58ca74f90c9fd7824c
Charles Henderson <Charles.henderson34@icloud.com>
2026-06-08 22:32:30 +0000

    Fix: Isolate only necessary files for deployment

--- my-production-vault/script/Deploy.s.sol
+++ my-production-vault/script/Deploy.s.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "src/NexusTokenRegistry.sol";
+import "../src/NexusTokenRegistry.sol";
 
 contract DeployScript is Script {
     function run() external {
         uint256 pk = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(pk);
         
-        // Deploying registry
+        // Ensure this address is correct
         new NexusTokenRegistry(0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE, 0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE);
         
         vm.stopBroadcast();
