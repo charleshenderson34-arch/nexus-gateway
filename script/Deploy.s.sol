cf0daee2dd12e20306c63787e2f362918087f699
YourUsername <your@email.com>
2026-06-08 23:23:12 +0000

    Update deployment script with target address

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,12 +3,19 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
 import "src/NexusTokenRegistry.sol";
+import "src/Assets.sol";
 
 contract DeployScript is Script {
     function run() external {
-        uint256 pk = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(pk);
-        new NexusTokenRegistry(0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE, 0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE);
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        address targetAddress = 0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE;
+        
+        vm.startBroadcast(deployerKey);
+        
+        // Using your specified address for both arguments
+        new NexusTokenRegistry(targetAddress, targetAddress);
+        new Assets();
+        
         vm.stopBroadcast();
     }
 }
