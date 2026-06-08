34bcf660eae63c82b62853d611660893f2b29902
YourUsername <your@email.com>
2026-06-08 23:15:28 +0000

    Add core deployment contracts and workflow

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,17 +1,14 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2026 Charles Timothy Henderson. All rights reserved.
 pragma solidity ^0.8.20;
-
 import "forge-std/Script.sol";
-import "../src/NexusTokenRegistry.sol";
+import "src/NexusTokenRegistry.sol";
+import "src/Assets.sol";
 
 contract DeployScript is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
-        
-        new NexusTokenRegistry(0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE, 0x2f4eBBD12848F7C3De8C6ED679734E3238f219EE);
-        
+        vm.startBroadcast();
+        new NexusTokenRegistry(msg.sender);
+        new Assets();
         vm.stopBroadcast();
     }
 }
