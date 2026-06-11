8358a37c9da977844440350509154096746deea7
YourUsername <your@email.com>
2026-06-11 09:13:35 +0000

    commit#

--- src/NexusTokenRegistry.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-contract NexusTokenRegistry {
-    address public issuerAuthority;
-    constructor(address _issuer) { issuerAuthority = _issuer; }
-}
